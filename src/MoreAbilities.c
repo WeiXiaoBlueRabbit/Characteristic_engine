8e49f38a04ff78d3ecc14e2fff60dea1645ee296
GoldenCaterpie <l>
2020-04-24 08:42:07 -0300

    扩充特性至0xFFFF个

--- src/MoreAbilities.c
+++ src/MoreAbilities.c
@@ -1,4 +1,18 @@
-int main(void)
+#include "defines.h"
+#include "static_references.h"
+
+u16 GetAbilityBySpecies(u16 species, u8 slot);
+static const u16 gPokeAbilities[][3];
+
+/**
+ * @用于获取精灵特性
+ */
+u16 GetAbilityBySpecies(u16 species, u8 slot)
 {
-    
+    return gPokeAbilities[species][slot];
 }
+
+static const u16 gPokeAbilities[][3] = {
+//  特性1   特性2   特性3
+    {0,     0,     0},
+};
